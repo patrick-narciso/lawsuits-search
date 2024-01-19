38e1d2f66d885890a61f84389f85e977247cd5c5
Patrick Narciso <patrick.ncsilva@gmail.com>
2024-01-18 21:31:30 -0300

    feat: cria entidade de Request

---
+++ src/entities/Request.ts
@@ -0,0 +1,15 @@
+export enum RequestStatus {
+  Completed = "completed",
+  Pending = "pending",
+  Created = "created",
+  Started = "started",
+  Cancelling = "cancelling",
+  Cancelled = "cancelled",
+}
+
+export enum SearchType {
+  CPF = "cpf",
+  CNPJ = "cnpj",
+  OAB = "oab",
+  CNJ = "lawsuit_cnj",
+}
